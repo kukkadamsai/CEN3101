5d43723b72f0c7b13d6a4df681e4421a7edfdd28
kukkadamsai <kukkadamsai@yahoo.com>
2019-09-06 14:41:34 -0400

    server.js first commit

--- server.js
+++ server.js
@@ -9,10 +9,18 @@ var listingData, server;
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
+  if(request.method === 'GET' && request.url === '/listings'){
+    response.end(JSON.stringify(listingData))
+  }
+  else{
+    response.statusCode = 404;
+    response.end('Bad gateway error');
+    //The code above will print the statement 'Bad gateway error' when the appropriate status code executes
+  }
+
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
     HINT: Explore the request object and its properties 
     HINT: Explore the response object and its properties
     https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
@@ -27,13 +35,23 @@ var requestHandler = function(request, response) {
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
+
+  if (err) throw err;
+  listingData = JSON.parse(data);
+  
+  var server = http.createServer(requestHandler);
+
+
+server.listen(port, function() {
+ 
+});
+console.log('server listening on: http://localhost:8080')
+
   /*
     This callback function should save the data in the listingData variable, 
     then start the server. 
-
     HINT: Check out this resource on fs.readFile
     //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-
     HINT: Read up on JSON parsing Node.js
    */
 
